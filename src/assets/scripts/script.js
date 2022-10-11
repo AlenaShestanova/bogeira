3c2ddc0e30c975b9b1a1c0199cce8bf0c3ab426c
a.shestanova <a.shestanova@rambler-co.ru>
2022-10-12 02:13:24 +0300

    add eng pages version

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -1,6 +1,48 @@
 function handleAchievementsItemClick(id) {
-
     const element = document.getElementById(`${id}`)
-
     element.classList.toggle('active')
+}
+
+function changeLinkColor(id){
+    const element = document.getElementById(`${id}`)
+    element.classList.toggle('change-color-link')
+}
+
+function handleLogoClick(){
+    const logo=document.getElementById('logo')
+
+    // const coords=logo.getBoundingClientRect()
+    //
+    // var i=coords.x
+    // for(i;i>0;i--){
+    //     console.log(i,'I')
+    //     setTimeout(()=>{
+    //         logo.style.left = i + 'px';
+    //     },5000)
+    // }
+
+
+    // let start = Date.now(); // запомнить время начала
+    //
+    // let timer = setInterval(function() {
+    //     // сколько времени прошло с начала анимации?
+    //     let timePassed = Date.now() - start;
+    //
+    //     if (timePassed >= 2000) {
+    //         clearInterval(timer); // закончить анимацию через 2 секунды
+    //         return;
+    //     }
+    //
+    //     // отрисовать анимацию на момент timePassed, прошедший с начала анимации
+    //     // draw(timePassed);
+    //
+    // }, 20);
+
+// в то время как timePassed идёт от 0 до 2000
+// left изменяет значение от 0px до 400px
+//     function draw(timePassed) {
+//         logo.style.left = timePassed / 5 + 'px';
+//     }
+
+
 }
