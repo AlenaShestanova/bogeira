315cecb2f9cec9cbc74b084e5c74e7f98cf002f8
AlenaShestanova <alenaneew@mail.ru>
2022-12-11 18:55:33 +0300

    add animation

--- src/assets/scripts/script.js
+++ src/assets/scripts/script.js
@@ -3,8 +3,33 @@ function handleAchievementsItemClick(id) {
     element.classList.toggle('active')
 }
 
-function changeLinkColor(id){
+function changeLinkColor(id) {
     const element = document.getElementById(`${id}`)
     element.classList.toggle('change-color-link')
 }
+
+function handleLogoClick() {
+    const containerElem = document.getElementById('main-container')
+    containerElem.classList.add('main-container-active')
+
+    const logoElem = document.getElementById('logo-primary-btn')
+    logoElem.classList.add('logo-animation')
+
+    primaryLogoHidden(logoElem)
+
+}
+
+function primaryLogoHidden(logo) {
+    setTimeout(() => {
+        if (logo) {
+            logo.classList.toggle('logo-hidden')
+            const iconInLogo=document.getElementById('icon-in-logo')
+            if(iconInLogo){
+                iconInLogo.style.visibility='visible'
+            }
+        }
+    }, 1500)
+}
+
+
 
